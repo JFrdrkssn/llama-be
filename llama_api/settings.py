dd1424e2b0ca36c9c1401369e16dc64c8faecae5
JFrdrkssn <johanf9401@gmail.com>
2022-05-11 18:41:45 +0000

    Add SECRET_KEY variable from env.py

--- llama_api/settings.py
+++ llama_api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3y!^scsxybw2s%ogjyfnl6u0ei(r*hdo5*2=r-kw=-1h_tro$3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
