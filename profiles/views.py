23997fd451a703ada09e0e6e55a5f85dc6892977
JFrdrkssn <johanf9401@gmail.com>
2022-05-12 14:13:26 +0000

    Add Profile detail view with permission to update if user is owner

--- profiles/views.py
+++ profiles/views.py
@@ -33,3 +33,16 @@ class ProfileList(generics.ListAPIView):
         'owner__following__created_at',
         'owner__followed__created_at',
     ]
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
