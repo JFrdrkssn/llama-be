46c9876b4527ed6cef010d3557b83fe087eb8a01
JFrdrkssn <johanf9401@gmail.com>
2022-05-13 12:25:12 +0000

    Add Category model

---
+++ categories/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """
+    Model for categories on the site.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=55)
+
+    class Meta:
+        """
+        Order category creation date in descending order, newest first.
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Category: {self.category}'
