f7d2a65da87dde26575246bd543829a99af07687
JFrdrkssn <johanf9401@gmail.com>
2022-05-13 12:31:55 +0000

    Add Django allauth to installed apps

--- llama_api/settings.py
+++ llama_api/settings.py
@@ -53,6 +53,11 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'profiles',
     'posts',
     'likes',
@@ -60,6 +65,8 @@ INSTALLED_APPS = [
     'comments',
     'categories',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
