21032addab5b8206fad6c291793156a1f7016552
JFrdrkssn <johanf9401@gmail.com>
2022-05-22 12:40:20 +0000

    Change name of Category model category field to name

--- categories/models.py
+++ categories/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     """
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    category = models.CharField(max_length=55)
+    name = models.CharField(max_length=55)
 
     class Meta:
         """
